959da2a3eaf3a1fc8a136322b80f753e9ad3f510
Rajdeep Kakar <rajdeepkakar@gmail.com>
2023-11-12 08:14:38 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	mode: "jit",
+	content: ["./app/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {},
+		// Add your custom configurations here
+	},
+	plugins: [],
+}
